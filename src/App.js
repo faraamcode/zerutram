8b554e3a7517f007465b4829896420b09405970a
faraamcode <abdulrasaqalagbede@gmail.com>
2023-10-26 04:56:56 +0100

    add data table

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg'
 import './App.css'
 import { Login } from './pages/Login'
 import DashboardIndex from './pages/Dashboard/'
+import CustomersPage from './pages/Dashboard/customers'
 
 function App () {
   return (
     <div>
-      <DashboardIndex />
+      <CustomersPage />
+      {/* <DashboardIndex /> */}
       {/* <Login /> */}
     </div>
   )
---
+++ src/pages/Dashboard/customers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DashboardMainComponent from '../../components/DashboardMain'
+import DataTable from '../../components/table'
+
+function CustomersPage () {
+  return (
+    <DashboardMainComponent>
+      <DataTable />
+    </DashboardMainComponent>
+  )
+}
+
+export default CustomersPage
